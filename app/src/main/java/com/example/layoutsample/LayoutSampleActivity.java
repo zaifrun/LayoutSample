f7b3f407cbcbc0a842e9c96c0fd66bebebaec712
Martin Knudsen <martinknud@gmail.com>
2016-01-21 13:38:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/layoutsample/LayoutSampleActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutsample;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutSampleActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen);
+    }
+
+}
