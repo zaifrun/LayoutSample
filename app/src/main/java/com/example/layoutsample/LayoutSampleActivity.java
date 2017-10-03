bc7d12bed75831e95ef6ab24781707df5066cd6c
zaifrun <martinknud@gmail.com>
2017-10-03 13:33:48 +0200

    feature branch: add comments

--- app/src/main/java/com/example/layoutsample/LayoutSampleActivity.java
+++ app/src/main/java/com/example/layoutsample/LayoutSampleActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class LayoutSampleActivity extends Activity {
+	//add comments in the feature branch.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
