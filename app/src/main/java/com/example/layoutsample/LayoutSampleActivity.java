4e09922df65de7e7bf81f3988c167e5166298169
zaifrun <martinknud@gmailc.om>
2018-03-14 13:53:48 +0100

    changed comments

--- app/src/main/java/com/example/layoutsample/LayoutSampleActivity.java
+++ app/src/main/java/com/example/layoutsample/LayoutSampleActivity.java
@@ -4,9 +4,7 @@ package com.example.layoutsample;
 
 import android.app.Activity;
 import android.os.Bundle;
-//added comments in the main branch.
 public class LayoutSampleActivity extends Activity {
-	//add comments in the feature branch.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
